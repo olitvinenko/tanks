afc62cc91b7fa6da0806aa692f12d62789fa3fe2
Oleksii Lytvynenko <alex.litvinenko.gm@gmail.com>
2021-07-01 19:39:46 +0300

    add tests ( should be rearranged asap )

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -22,7 +22,155 @@ glm::mat4 camera(float Translate, const glm::vec2& Rotate)
     return Projection * View * Model;
 }
 
+#include <GameLoop.h>
+
+#include <iostream>
+
+struct FixedUpd : public IFixedUpdatable
+{
+    int i = 0;
+    
+    FixedUpd(int ii) : i(ii)
+    {
+    }
+    
+    ~FixedUpd()
+    {
+    }
+    
+    void FixedUpdate(float fixedDeltaTime) override
+    {
+        std::cout << "FixedUpdate " << fixedDeltaTime << std::endl;
+    }
+};
+
+struct Upd : public IUpdatable
+{
+    void Update(float fixedDeltaTime) override
+    {
+        std::cout << "Update " << fixedDeltaTime << std::endl;
+    }
+};
+
+struct Rend : public IRenderable
+{
+    void Render(float fixedDeltaTime) override
+    {
+        std::cout << "Render " << fixedDeltaTime << std::endl;
+    }
+};
+
+#include <thread>
+#include <chrono>
+
+//#include <base/IWindow.h>
+#include "glfw/GlfwWindow.h"
+#include "glfw/GlfwInput.h"
+#include "glfw/GlfwClipboard.h"
+
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+struct InputListener : public IInputListener
+{
+    virtual void OnMouseButtonDown(MouseButton button) {
+        std::cout << "OnMouseButtonDown" << std::endl;
+    }
+    virtual void OnMouseButtonUp(MouseButton button) {
+        std::cout << "OnMouseButtonUp" << std::endl;
+    }
+    
+    virtual void OnMousePosition(float x, float y) {
+        std::cout << "OnMousePosition" << std::endl;
+    }
+    virtual void OnMouseScrollOffset(float x, float y) {
+        std::cout << "OnMouseScrollOffset" << std::endl;
+    }
+    
+    virtual void OnCharacter(char character) {
+        std::cout << "OnCharacter" << std::endl;
+    }
+    
+    virtual void OnKeyDown(UI::Key key) {
+        std::cout << "OnKeyDown" << std::endl;
+    }
+    virtual void OnKey(UI::Key key) {
+        std::cout << "OnKey" << std::endl;
+    }
+    virtual void OnKeyUp(UI::Key key) {
+        std::cout << "OnKeyUp" << std::endl;
+    }
+};
+
+#include <base/Keys.h>
+
+#include <string>
 int main(int, const char**)
 {
+    auto window = std::make_shared<GlfwWindow>("test window", 940, 640, false);
+    
+    InputListener listener;
+    IInput* input = new GlfwInput(window.get());
+    input->AddListener(&listener);
+    
+    IClipboard* cl = new GlfwClipboard(window.get());
+    std::string clS;
+    
+    for (window->PollEvents(); !window->ShouldClose();)
+    {
+        input->Read();
+        
+        glClearColor(0, 0.4, 0, 1);
+        glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        if (window->IsFocused())
+        {
+            clS = cl->GetText();
+            if (!clS.empty())
+            {
+                std::cout << "clip " << clS << std::endl;
+                clS.clear();
+                cl->SetText("");
+            }
+            
+            if (input->GetKey(UI::Key::A))
+            {
+                std::cout << "aaaaaa " << clS << std::endl;
+            }
+        }
+        
+        window->SwapBuffers();
+        input->Clear();
+    }
+    
+    
+    return 0;
+    
+    
+    static_assert(std::is_base_of<IFixedUpdatable, FixedUpd>::value, "");
+    
+    GameLoop gl;
+    
+    auto fixedUpd = gl.Add<FixedUpd>(10);
+    gl.Add<Rend>();
+    gl.Add<Upd>();
+    
+    gl.Start();
+    for (int i = 0; i < 1000000; ++i)
+    {
+        gl.Tick();
+        
+//        for (int j = 0; j < 1000000; ++j)
+//        { }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    
+    
+    std::weak_ptr<FixedUpd> upd2 = fixedUpd;
+    gl.Remove(fixedUpd.lock().get());
+    gl.Remove(fixedUpd);
+
 	return 0;
 }
